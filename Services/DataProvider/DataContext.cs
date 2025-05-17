48c1fee3a8e801241d23276e83fadedf05317cc6
tuurash <turasmimoo@gmail.com>
2025-05-17 13:34:37 +0600

    Add project files.

---
+++ Services/DataProvider/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+using ModelsProvider;
+
+namespace DataProvider;
+
+public class DataContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+
+    protected readonly IConfiguration Configuration;
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // Move to AppSettings.json 
+        options.UseSqlite(Configuration.GetConnectionString("Data Source=LocalDatabase.db"));
+    }
+
+}
+
---
+++ Services/ModelsProvider/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace ModelsProvider
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+        public UserRole Role { get; set; } = UserRole.Employee;
+    }
+
+
+    public enum UserRole
+    {
+        Admin,
+        Manager,
+        Employee
+    }
+
+}
